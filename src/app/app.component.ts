421a2cd299ee271b4fb1a1f742ae38923d7b5542
Nikhil Vinnakoti <you@example.com>
2020-06-05 14:45:15 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angulartask';
+  profileData = [
+    {
+      type: "FREE",
+      rate: "$0/month",
+      userType: "Single User",
+      Storage: "5GB Storage",
+      Projects: "Unlimited Public Projects",
+      Access: "Community Access",
+      Project: "Unlimited Private Projects",
+      Support: "Dedicated Phone Support",
+      Domain: "Free Subdomain",
+      Status: "Monthly Status Reports"
+    },
+    {
+      type: "PLUS",
+      rate: "$9/month",
+      userType: "5 Users",
+      Storage: "50GB Storage",
+      Projects: "Unlimited Public Projects",
+      Access: "Community Access",
+      Project: "Unlimited Private Projects",
+      Support: "Dedicated Phone Support",
+      Domain: "Free Subdomain",
+      Status: "Monthly Status Reports"
+    },
+    {
+      type: "PRO",
+      rate: "$45/month",
+      userType: "Unlimited Users",
+      Storage: "150GB Storage",
+      Projects: "Unlimited Public Projects",
+      Access: "Community Access",
+      Project: "Unlimited Private Projects",
+      Support: "Dedicated Phone Support",
+      Domain: "Unlimited Free Subdomains",
+      Status: "Monthly Status Reports"
+    }
+  ]
+}
